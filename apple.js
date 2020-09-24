fb9df95523a6e4fbbc063629b7e752acf6793e8a
allef-rocha <67357475+allef-rocha@users.noreply.github.com>
2020-09-24 10:33:47 -0300

    Update apple.js

--- apple.js
+++ apple.js
@@ -1,23 +1,23 @@
 function Apple(x){
     this.r = 25
     this.pos = createVector(x, random(this.r, height - this.r))
+    this.x_off = 0
+    this.y_off = 0
 
 
     this.show = function(){
         fill(158, 20, 20, 255)
         stroke(255)
         strokeWeight(1)
-        ellipse(this.pos.x, this.pos.y, this.r)
+        ellipse(this.pos.x+this.x_off, this.pos.y+this.y_off, this.r)
         noStroke()
     }
 
     this.update = function(){
         angleMode(RADIANS)
         let a = count*0.1
-        let x_off=10*sin(a*2)
-        let y_off=10*cos(a/2)
-        this.pos.x += x_off
-        this.pos.y += y_off
+        this.x_off=15*sin(a*2)
+        this.y_off=40*cos(a/2)
         this.pos.x -= pipeSpeed
     }
 
