d58e509d823ea9af141e6de2d528d6e83cc7b0b2
allef-rocha <67357475+allef-rocha@users.noreply.github.com>
2020-09-24 00:19:52 -0300

    Update apple.js

--- apple.js
+++ apple.js
@@ -12,8 +12,8 @@ function Apple(x){
 
     this.update = function(){
         let a = map(count%60,0,59,-PI,PI)
-        this.pos.x+=2*sin(a)
-        this.pos.y+=5*cos(3*a)
+        this.pos.x+=4*sin(2*a)
+        this.pos.y+=25*cos(a/2)
         this.pos.x -= pipeSpeed
     }
 
